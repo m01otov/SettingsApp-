55e98c26771958e84830efc08bd905034ebde984
m01otov <67689912+m01otov@users.noreply.github.com>
2021-10-16 20:09:53 +0500

    Added table skeleton

--- SettingsApp/ViewController.swift
+++ SettingsApp/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let tableView: UITableView = {
+           let table = UITableView(frame: .zero, style: .grouped)
+           table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+           return table
 
+       }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Settings"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
+    //Добавим функции которые требуются для таблицы
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 0   }
 
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = "Test"
+            return cell
 
+        }
 }
-
